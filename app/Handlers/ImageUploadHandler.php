60b880d8339447ea9aadf60712876285e715e7db
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-07 12:17:19 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,7 +2,8 @@
 
 namespace App\Handlers;
 
-use Illuminate\Support\Str;
+use Image;
+// 上面的用法是采用Facades注册了别名，等价于 use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
@@ -16,7 +17,7 @@ class ImageUploadHandler
      * @param [string] $file_prefix 文件名的前缀
      * @return array|boolean
      */
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width=false)
     {
         // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
@@ -36,13 +37,37 @@ class ImageUploadHandler
 
         // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
         // 值如：1_1493521050_7BVc9v9ujP.png
-        $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+        $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
 
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $filename);
 
+        if ($max_width && $extension != 'gif') {
+            // 裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        // 先实例化，传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
