c808119eea66d9f5d24e9a5a7848262f71515ee7
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-07 18:03:37 +0800

    topics表添加注释

--- database/migrations/2020_01_07_162808_create_topics_table.php
+++ database/migrations/2020_01_07_162808_create_topics_table.php
@@ -2,25 +2,28 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
-class CreateTopicsTable extends Migration 
+class CreateTopicsTable extends Migration
 {
 	public function up()
 	{
 		Schema::create('topics', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->index();
-            $table->text('body');
-            $table->bigInteger('user_id')->unsigned()->index();
-            $table->integer('category_id')->unsigned()->index();
-            $table->integer('reply_count')->unsigned()->default(0);
-            $table->integer('view_count')->unsigned()->default(0);
-            $table->integer('last_reply_user_id')->unsigned()->default(0);
-            $table->integer('order')->unsigned()->default(0);
-            $table->text('except')->nullable();
-            $table->string('slug')->nullable();
+            $table->string('title')->index()->comment('帖子标题');
+            $table->text('body')->comment('帖子内容');
+            $table->bigInteger('user_id')->unsigned()->index()->comment('用户ID');
+            $table->integer('category_id')->unsigned()->index()->comment('分类ID');
+            $table->integer('reply_count')->unsigned()->default(0)->comment('回复数量');
+            $table->integer('view_count')->unsigned()->default(0)->comment('查看总数');
+            $table->integer('last_reply_user_id')->unsigned()->default(0)->comment('最后回复的用户ID');
+            $table->integer('order')->unsigned()->default(0)->comment('排序');
+            $table->text('except')->nullable()->comment('文章摘要');
+            $table->string('slug')->nullable()->comment('SEO 友好的 URI');
             $table->timestamps();
         });
+
+        DB::statement("ALTER TABLE `topics` comment '帖子'");
 	}
 
 	public function down()
