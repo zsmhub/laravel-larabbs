c318b385fec40930eb9e6145df6131ee5c46195a
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-08 16:06:58 +0800

    显示帖子

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="@yield('description', 'LarBBS 爱好者社区')" />
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
