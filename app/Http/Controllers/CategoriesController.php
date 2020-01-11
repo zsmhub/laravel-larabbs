e9ead30f83f70618c86f2afbb74948e937e38470
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-11 22:33:10 +0800

    边栏资源推荐

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, User $user)
+    public function show(Category $category, Request $request, User $user, Link $link)
     {
         $topics = Topic::withOrder($request->order)->where('category_id', $category->id)->paginate(20);
-
         $active_users = $user->getActiveUsers();
-
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        $links = $link->getAllCached();
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
---
+++ app/Models/Link.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+class Link extends Model
+{
+    protected $fillable = ['title', 'link'];
+
+    public $cache_key = 'larabbs_links';
+    protected $cache_expire_in_seconds = 1440 * 60;
+
+    public function getAllCached()
+    {
+        return Cache::remember($this->cache_key, $this->cache_expire_in_seconds, function() {
+            return $this->all();
+        });
+    }
+}
