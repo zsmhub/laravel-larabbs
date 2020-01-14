1d2383d688a82c30d2ca55f82c11cb817d648b0d
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-14 16:34:16 +0800

    verificationCode store

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller as BaseController;
+use Dingo\Api\Routing\Helpers;
+
+class Controller extends BaseController
+{
+    use Helpers;
+}
