37f57b064fb52c00cbd9f2ce3c550b335d059957
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-18 16:43:13 +0800

    topics update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,17 @@ class AppServiceProvider extends ServiceProvider
         if (app()->isLocal()) {
             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
         }
+
+        // API 的异常处理，防止暴露代码细节
+        \API::error(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $exception) {
+            // throw new \Symfony\component\HttpKernel\Exception\HttpException(404, '404 Not Found');
+            abort(404);
+        });
+
+        // API 的权限控制状态码控制
+        \API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception) {
+            abort(403, $exception->getMessage());
+        });
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array',
+    'middleware' => ['serializer:array', 'bindings'],
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -66,9 +66,13 @@ $api->version('v1', [
             // 图片资源
             $api->post('images', 'ImagesController@store')
                 ->name('api.images.store');
+
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
+            // 修改话题
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
         });
     });
 });
