a482cf7af549802c6298ff500c6af6ad92c1726c
Insomnia Zhang <zhangshimiande@gmail.com>
2020-02-29 21:22:24 +0800

    locale

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller as BaseController;
 use Dingo\Api\Routing\Helpers;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Controller extends BaseController
 {
     use Helpers;
+
+    public function errorResponse($statusCode, $message=null, $code=0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
